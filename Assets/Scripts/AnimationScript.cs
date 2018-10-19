d2571a50bfd5086009421d9b04804fbfbee876d4
LEARNET\joel.carlsson1 <joel.carlsson@elev.ga.lbs.se>
2018-10-19 15:35:57 +0200

    Made some animations

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    public Animator anim;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("VadNiVill", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetBool("VadNiVill", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("Triggered");
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetInteger("Najs", 16);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetInteger("Najs", 15);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rBody;
+    //Add force = Putta saker, relativ till skärmen
+    //Add relative force = Putta saker, relativ till gubben
+    //Add torque = rotation
+    //Move position/roation = sätter rotation/position
+    //Velocity = Vilken hastighet objektet har
+    //Angular velocity = Vilken hastighet snurrar objektet
+    // Use this for initialization
+    void Start()
+    {
+        //rBody.AddForce(new Vector2(0, 20f), ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }       
+    }
+    //Allt med fysik ska in här, 50 gånger i sekund
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+        rBody.AddForce(new Vector2(10f, 0f));   
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rBody.AddForce(new Vector2(-10f, 0f));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rBody.AddForce(new Vector2(0f, 10f));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rBody.AddForce(new Vector2(0f, -10f));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("You suck");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
+
